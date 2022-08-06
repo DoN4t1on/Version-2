43845d0f573580c4f53876da8d73e7afa4920af2
macOS <62692034+me-saeed@users.noreply.github.com>
2022-08-06 18:11:34 +0500

    config file

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //endPoint: 'http://localhost:5009/api',
+  endPoint: 'https://app.lokalpetition.de/api',
+  mapAPiKey: 'AIzaSyCkDkwcM-UT4Q6k5gfhFXeoN7JRAQek-PI',
+
+  ImageEndPoint: 'https://app.lokalpetition.de/readfiles/',
+
+  localToken: 'localpetition',
+};
