1a8750ae0048c0c27fb9f2df12701c314d96580e
Felix Scherf <contact@matrix.engineer>
2022-08-31 02:36:07 +0200

    Changed color of the cookie privacy link

--- src/index.js
+++ src/index.js
@@ -250,7 +250,7 @@ ReactDOM.render(
         Diese Webseite benutzt Cookies.{' '}
         <a
           href='https://app.lokalspende.org/datenschutz'
-          style={{ color: 'blue' }}
+          style={{ color: 'lightblue' }}
         >
           Hier
         </a>{' '}
